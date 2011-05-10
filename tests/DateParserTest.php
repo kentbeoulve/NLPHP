bab2f3f93ea848a0b5aac0220210eecdedfad04a
Charles-Edouard Coste <cc@synap.fr>
2011-05-11 00:24:11 +0200

    Adding a test case for the phrase "le jeudi 12 mai 2011"

---
+++ tests/DateParserTest.php
@@ -0,0 +1,35 @@
+<?php
+class DateParserTest extends PHPUnit_Framework_TestCase
+{
+    private $grammar;
+
+    public function setUp()
+    {
+        $this->grammar = new ChomskyNormalForm();
+
+        // terminal symbols
+        $this->grammar->addRule( new RewriteRule( "ARTICLE", "le" ) );
+        $this->grammar->addRule( new RewriteRule( "JOUR", "jeudi" ) );
+        $this->grammar->addRule( new RewriteRule( "MOIS", "mai" ) );
+        $this->grammar->addRule( new RewriteRule( "NOMBRE", "12" ) );
+        $this->grammar->addRule( new RewriteRule( "NUM_JOUR", "12" ) );
+        $this->grammar->addRule( new RewriteRule( "NOMBRE", "2011" ) );
+        $this->grammar->addRule( new RewriteRule( "NUM_ANNEE", "2011" ) );
+
+        $this->grammar->addRule( new RewriteRule( "ARTICLE_JOUR", "ARTICLE", "JOUR" ) );
+        $this->grammar->addRule( new RewriteRule( "ARTICLE_JOUR_NUM", "ARTICLE_JOUR", "NUM_JOUR" ) );
+
+        $this->grammar->addRule( new RewriteRule( "MOIS_ANNEE", "MOIS", "NUM_ANNEE" ) );
+
+        $this->grammar->addRule( new RewriteRule( "DATE", "ARTICLE_JOUR_NUM", "MOIS_ANNEE" ) );
+
+        $this->cyk = new CYKAlgorithm( $this->grammar );
+        $this->cyk->load( array( 'le', 'jeudi', '12', 'mai', '2011' ) );
+    }
+
+    public function testA1()
+    {
+        $this->assertTrue( $this->cyk->validate( 'DATE' ) );
+    }
+}
+?>
