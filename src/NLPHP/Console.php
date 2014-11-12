a23f3b0d1511a79fcf57fe67c9269b039209eb4f
Charles-Edouard Coste <cc@synap.fr>
2014-11-12 17:27:39 +0100

    Simplify Console class

--- src/NLPHP/Console.php
+++ src/NLPHP/Console.php
@@ -9,50 +9,89 @@ use ezcConsoleTable;
  */
 class Console
 {
-    public static function main()
+    private $output;
+    private $table;
+
+    public function __construct()
     {
-        $gram = new ChomskyNormalForm();
+        $this->output = new ezcConsoleOutput();
+        $this->output->formats->headBorder->color = 'blue';
+        $this->output->formats->normalBorder->color = 'gray';
+        $this->output->formats->headContent->color = 'blue';
+        $this->output->formats->headContent->style = array('bold');
 
-        $gram->addRule(new RewriteRule("DET", "le"));
-        $gram->addRule(new RewriteRule("DET", "la"));
-        $gram->addRule(new RewriteRule("SN", "chat"));
-        $gram->addRule(new RewriteRule("SN", "souris"));
-        $gram->addRule(new RewriteRule("V", "mange"));
-        $gram->addRule(new RewriteRule("GN", "DET", "SN"));
-        $gram->addRule(new RewriteRule("GV", "V", "GN"));
-        $gram->addRule(new RewriteRule("PH", "GN", "GV"));
+        $this->table = new ezcConsoleTable($this->output, 78);
 
-        $test = array('le', 'chat', 'mange', 'la', 'souris');
+        $this->table->options->defaultBorderFormat = 'normalBorder';
 
-        $cyk = new CYKAlgorithm($gram);
+        $this->table[0]->borderFormat = 'headBorder';
+        $this->table[0]->format       = 'headContent';
+        $this->table[0]->align        = ezcConsoleTable::ALIGN_CENTER;
+    }
 
-        $cyk->load($test);
-
-        $output = new ezcConsoleOutput();
-
-        $output->formats->headBorder->color = 'blue';
-        $output->formats->normalBorder->color = 'gray';
-        $output->formats->headContent->color = 'blue';
-        $output->formats->headContent->style = array('bold');
-
-        $table = new ezcConsoleTable($output, 78);
-        $table->options->defaultBorderFormat = 'normalBorder';
-
-        $table[0]->borderFormat = 'headBorder';
-        $table[0]->format       = 'headContent';
-        $table[0]->align        = ezcConsoleTable::ALIGN_CENTER;
-
-        foreach ($cyk->matrix as $row => $cells) {
+    public function outputTable(array $rows)
+    {
+        foreach ($rows as $row => $cells) {
 
             foreach ($cells as $k => $cell) {
 
-                $table[$row][$k]->content = implode('|', $cell);
+                $this->table[$row][$k]->content = $cell;
             }
         }
 
-        $output->outputLine('eZ components team:');
-        $table->outputTable();
-        $output->outputLine();
+        $this->table->outputTable();
+        $this->output->outputLine();
+    }
+
+    public function outputCYKMatrix($matrix)
+    {
+        $table = array();
+
+        foreach ($matrix as $row => $cells) {
+
+            foreach ($cells as $k => $cell) {
+
+                $table[$row][$k] = implode('|', $cell);
+            }
+        }
+
+        $this->outputTable($table);
+    }
+
+    public static function main()
+    {
+        // 1. Instanciate a generic ChomskyNormalForm
+        $grammar = new ChomskyNormalForm();
+
+        // 2. Configure the grammar
+        $rules = array(
+            new RewriteRule("DET", "le"),
+            new RewriteRule("DET", "la"),
+            new RewriteRule("SN", "chat"),
+            new RewriteRule("SN", "souris"),
+            new RewriteRule("V", "mange"),
+            new RewriteRule("GN", "DET", "SN"),
+            new RewriteRule("GV", "V", "GN"),
+            new RewriteRule("PH", "GN", "GV")
+        );
+
+        foreach ($rules as $rule) {
+
+            $gram->addRule($rule);
+        }
+ 
+        // 3. Instanciate a CYK algorithm matrix
+        $cyk = new CYKAlgorithm($grammar);
+
+        // 4. Load a phrase in the matrix
+        $cyk->load(
+            array('le', 'chat', 'mange', 'la', 'souris')
+        );
+
+        // 5. Output the matrix
+        $this->outputCYKMatrix($cyk->matrix);
+
+        // new line
+        $this->output->outputLine();
     }
 }
-Console::main();
